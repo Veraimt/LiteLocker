5e22cd6b2b6dd8b571b15688ef500581326be916
Veraimt <maxmarasas@outlook.de>
2022-08-15 16:53:33 +0200

    Enabled Mixins for Common and Forge; Modified build scripts

--- Common/src/main/java/de/veraimt/litelocker/LiteLocker.java
+++ Common/src/main/java/de/veraimt/litelocker/LiteLocker.java
@@ -1,14 +1,14 @@
 package de.veraimt.litelocker;
 
 import de.veraimt.litelocker.platform.services.IPlatformHelper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class LiteLocker {
 
     public static final String MOD_ID = "litelocker";
     public static final String MOD_NAME = "LiteLocker";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
+    public static final Logger LOGGER = LogManager.getLogger();
 
     public static void init() {
         LOGGER.info("Loading {} on {} Modding Platform", MOD_NAME, IPlatformHelper.PLATFORM.getPlatformName());
---
+++ Common/src/main/java/de/veraimt/litelocker/mixin/TestMixin.java
@@ -0,0 +1,17 @@
+package de.veraimt.litelocker.mixin;
+
+import de.veraimt.litelocker.LiteLocker;
+import net.minecraft.client.gui.screens.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class TestMixin {
+
+    @Inject(at = @At("HEAD"), method = "init()V")
+    private void init(CallbackInfo info) {
+        LiteLocker.LOGGER.info("Hello from Mixin");
+    }
+}
